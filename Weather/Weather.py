3c7cbedac6565990b241f8011b23db0c5937bebe
HP <nguyenvonhatthuan@gmail.com>
2025-12-01 21:39:46 +0700

    temporary get address cord

--- Weather/Weather.py
+++ Weather/Weather.py
@@ -14,3 +14,17 @@ def get_weather(lat, lon):
     return response.json()
 
 data = get_weather(21.03, 105.85)  # Hà Nội
+
+# Get Place Coordinates
+def get_address_coordinates(address):
+    url = "https://nominatim.openstreetmap.org/search"
+    params = {'q': address, 'format': 'json'}
+    headers = {'User-Agent': 'PythonApp/1.0 (your_email@example.com)'}
+    response = requests.get(url, params=params, headers=headers)
+    data = response.json()
+    if data:
+        lat = float(data[0]['lat'])
+        lon = float(data[0]['lon'])
+        return lat, lon
+    else:
+        return None, None
